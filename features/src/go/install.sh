b04fd197e71caf5b2566253cc33c9538a6d22b3d
Roemer <r.baeriswyl@gmail.com>
2025-08-13 15:02:35 +0000

    Also handle aarch64 as arm64

--- features/src/go/install.sh
+++ features/src/go/install.sh
@@ -1,6 +1,6 @@
-case $(uname -m) in
+case $(uname -m | tr '[:upper:]' '[:lower:]') in
   x86_64*) ARCH=amd64 ;;
-  arm*)    ARCH=arm64 ;;
+  arm*|aarch64*) ARCH=arm64 ;;
   *) echo "Unsupported architecture: $(uname -m)" >&2; exit 1 ;;
 esac
 
