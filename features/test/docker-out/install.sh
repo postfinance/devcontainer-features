860dffad23b06942706ee380c83b3d9bb598e4fc
Roemer <r.baeriswyl@gmail.com>
2025-08-14 11:49:49 +0000

    Added docker-out tests

---
+++ features/test/docker-out/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+[[ -f "$(dirname "$0")/../functions.sh" ]] && source "$(dirname "$0")/../functions.sh"
+[[ -f "$(dirname "$0")/functions.sh" ]] && source "$(dirname "$0")/functions.sh"
+
+check_version "$(docker version -f '{{.Client.Version}}')" "27.2.1"
