30420e04092ad14a39ac4d5c7a59cd7d828eb2b4
mailaenderli <raphael.mailaender@gmail.com>
2025-09-10 14:43:02 +0000

    refactor: move GetOsInfo to installer package

--- features/src/nginx/installer.go
+++ features/src/nginx/installer.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"builder/installer"
 	"flag"
 	"fmt"
 	"os"
 	"regexp"
-	"strings"
 
 	"github.com/roemer/gover"
 )
@@ -45,7 +43,7 @@ func runMain() error {
 	installer.HandleOverride(downloadUrl, "https://nginx.org", "nginx-download-url")
 
 	// Fetch the os info
-	osInfo, err := getOsInfo()
+	osInfo, err := installer.Tools.System.GetOsInfo()
 	if err != nil {
 		return fmt.Errorf("failed getting OS info: %w", err)
 	}
@@ -66,33 +64,10 @@ func runMain() error {
 // Implementation
 //////////
 
-type osInfo struct {
-	vendor   string
-	codename string
-}
-
-func getOsInfo() (*osInfo, error) {
-	f, err := os.Open("/etc/os-release")
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	s := bufio.NewScanner(f)
-	infoMap := map[string]string{}
-	for s.Scan() {
-		parts := strings.SplitN(s.Text(), "=", 2)
-		infoMap[parts[0]] = parts[1]
-	}
-	return &osInfo{
-		vendor:   infoMap["ID"],
-		codename: infoMap["VERSION_CODENAME"],
-	}, nil
-}
-
 type nginxComponent struct {
 	*installer.ComponentBase
 	stableOnly  bool
-	osInfo      *osInfo
+	osInfo      *installer.OsInfo
 	downloadUrl string
 }
 
@@ -129,7 +104,7 @@ func (c *nginxComponent) InstallVersion(version *gover.Version) error {
 	if err != nil {
 		return err
 	}
-	debName := fmt.Sprintf("nginx_%s~%s_%s.deb", version.Raw, c.osInfo.codename, archPart)
+	debName := fmt.Sprintf("nginx_%s~%s_%s.deb", version.Raw, c.osInfo.Codename, archPart)
 	urls = append(urls, c.getStableUrl()+debName)
 	if !c.stableOnly {
 		urls = append(urls, c.getMainlineUrl()+debName)
@@ -159,18 +134,18 @@ func (c *nginxComponent) InstallVersion(version *gover.Version) error {
 }
 
 func (c *nginxComponent) getStableUrl() string {
-	return fmt.Sprintf("%s/packages/%s/pool/nginx/n/nginx/", c.downloadUrl, c.osInfo.vendor)
+	return fmt.Sprintf("%s/packages/%s/pool/nginx/n/nginx/", c.downloadUrl, c.osInfo.Vendor)
 }
 
 func (c *nginxComponent) getMainlineUrl() string {
-	return fmt.Sprintf("%s/packages/mainline/%s/pool/nginx/n/nginx/", c.downloadUrl, c.osInfo.vendor)
+	return fmt.Sprintf("%s/packages/mainline/%s/pool/nginx/n/nginx/", c.downloadUrl, c.osInfo.Vendor)
 }
 
 func (c *nginxComponent) lineExtractFunc(url, line string) (*gover.Version, error) {
 	if match := indexLineRegexp.FindStringSubmatch(line); match != nil {
 		versionString := match[2]
 		codename := match[3]
-		if codename == c.osInfo.codename {
+		if codename == c.osInfo.Codename {
 			version := gover.MustParseVersionFromRegex(versionString, versionRegexp)
 			return version, nil
 		}
