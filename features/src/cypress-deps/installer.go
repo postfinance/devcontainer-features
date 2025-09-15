7e16de3f7ec77053e095f1994ba01f583b8a1975
mailaenderli <raphael.mailaender@gmail.com>
2025-09-15 07:12:55 +0000

    feat(cypress-deps): update dependency installation for Debian and Ubuntu variants

--- features/src/cypress-deps/installer.go
+++ features/src/cypress-deps/installer.go
@@ -15,16 +15,49 @@ func main() {
 
 func runMain() error {
 	fmt.Println("Installing Cypress Dependencies")
-	return installer.Tools.Apt.InstallDependencies(
-		"libgtk2.0-0",
-		"libgtk-3-0",
+	osInfo, err := installer.Tools.System.GetOsInfo()
+	if err != nil {
+		return fmt.Errorf("failed to get OS info: %w", err)
+	}
+
+	// Common dependencies for all supported distros
+	commonDeps := []string{
 		"libgbm-dev",
 		"libnotify-dev",
 		"libnss3",
 		"libxss1",
-		"libasound2",
 		"libxtst6",
 		"xauth",
 		"xvfb",
-	)
+	}
+
+	var deps []string
+
+	debianTrixieOrNewer := osInfo.Vendor == "debian" && func() bool {
+		var major int
+		fmt.Sscanf(osInfo.VersionId, "%d", &major)
+		return major >= 13
+	}()
+
+	ubuntuNobleOrNewer := osInfo.Vendor == "ubuntu" && func() bool {
+		var major int
+		fmt.Sscanf(osInfo.VersionId, "%d", &major)
+		return major >= 24
+	}()
+
+	if debianTrixieOrNewer || ubuntuNobleOrNewer {
+		deps = append([]string{
+			"libgtk2.0-0t64",
+			"libgtk-3-0t64",
+			"libasound2t64",
+		}, commonDeps...)
+	} else {
+		deps = append([]string{
+			"libgtk2.0-0",
+			"libgtk-3-0",
+			"libasound2",
+		}, commonDeps...)
+	}
+
+	return installer.Tools.Apt.InstallDependencies(deps...)
 }
--- features/test/cypress-deps/packages_installed.sh
+++ features/test/cypress-deps/packages_installed.sh
@@ -4,13 +4,23 @@ set -e
 [[ -f "$(dirname "$0")/../functions.sh" ]] && source "$(dirname "$0")/../functions.sh"
 [[ -f "$(dirname "$0")/functions.sh" ]] && source "$(dirname "$0")/functions.sh"
 
-check_package_installed "libgtk2.0-0"
-check_package_installed "libgtk-3-0"
+# Detect distro and codename
+. /etc/os-release
+
+if { [[ "$ID" = "debian" && "$VERSION_CODENAME" = "trixie" ]] || [[ "$ID" = "ubuntu" && "$VERSION_CODENAME" = "noble" ]]; }; then
+    check_package_installed "libgtk2.0-0t64"
+    check_package_installed "libgtk-3-0t64"
+    check_package_installed "libasound2t64"
+else
+    check_package_installed "libgtk2.0-0"
+    check_package_installed "libgtk-3-0"
+    check_package_installed "libasound2"
+fi
+
 check_package_installed "libgbm-dev"
 check_package_installed "libnotify-dev"
 check_package_installed "libnss3"
 check_package_installed "libxss1"
-check_package_installed "libasound2"
 check_package_installed "libxtst6"
 check_package_installed "xauth"
 check_package_installed "xvfb"
