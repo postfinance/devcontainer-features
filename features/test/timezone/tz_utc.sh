e8c46bcf2e9a9e5863d873639e6c5c03b99d4a9f
Roemer <r.baeriswyl@gmail.com>
2025-10-16 14:52:50 +0000

    feat: Added timezone feature

---
+++ features/test/timezone/tz_utc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+[[ -f "$(dirname "$0")/../functions.sh" ]] && source "$(dirname "$0")/../functions.sh"
+[[ -f "$(dirname "$0")/functions.sh" ]] && source "$(dirname "$0")/functions.sh"
+
+check_version "$(date +%Z)" "UTC"
+check_version "$(cat /etc/timezone)" "Etc/UTC"
