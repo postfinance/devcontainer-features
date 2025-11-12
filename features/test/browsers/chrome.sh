98cac5989a7531256966f989aa3baab46e7f2aa1
Roemer <r.baeriswyl@gmail.com>
2025-11-12 10:55:12 +0000

    chore: Update chrome version for the tests

--- features/test/browsers/chrome.sh
+++ features/test/browsers/chrome.sh
@@ -4,7 +4,7 @@ set -e
 [[ -f "$(dirname "$0")/../functions.sh" ]] && source "$(dirname "$0")/../functions.sh"
 [[ -f "$(dirname "$0")/functions.sh" ]] && source "$(dirname "$0")/functions.sh"
 
-check_version "$(google-chrome --version)" "Google Chrome 130.0.6723.69"
+check_version "$(google-chrome --version)" "Google Chrome 142.0.7444.162"
 
 check_command_not_exists "chrome"
 check_command_not_exists "firefox"
