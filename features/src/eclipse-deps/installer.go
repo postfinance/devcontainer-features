004291444e6796d6600ac9251ff14fa6b10332b8
Raphael Meyer <raphael.meyer.1@postfinance.ch>
2026-08-14 18:00:48 +0200

    chore(eclipse-deps): install webkit version based on debian/ubuntu version

--- features/src/eclipse-deps/installer.go
+++ features/src/eclipse-deps/installer.go
@@ -15,8 +15,22 @@ func main() {
 
 func runMain() error {
 	fmt.Println("Installing Eclipse Dependencies")
+
+	osInfo, err := installer.Tools.System.GetOsInfo()
+	if err != nil {
+		return fmt.Errorf("failed to get OS info: %w", err)
+	}
+
+	debianTrixieOrNewer := osInfo.IsDebian() && osInfo.MajorVersion() >= 13
+	ubuntuNobleOrNewer := osInfo.IsUbuntu() && osInfo.MajorVersion() >= 24
+
+	webkitPackage := "libwebkit2gtk-4.0-37"
+	if debianTrixieOrNewer || ubuntuNobleOrNewer {
+		webkitPackage = "libwebkit2gtk-4.1-0"
+	}
+
 	return installer.Tools.Apt.InstallDependencies(
 		"libswt-gtk-4-jni",
-		"libwebkit2gtk-4.0-37",
+		webkitPackage,
 	)
 }
--- features/test/eclipse-deps/packages_installed.sh
+++ features/test/eclipse-deps/packages_installed.sh
@@ -4,5 +4,13 @@ set -e
 [[ -f "$(dirname "$0")/../functions.sh" ]] && source "$(dirname "$0")/../functions.sh"
 [[ -f "$(dirname "$0")/functions.sh" ]] && source "$(dirname "$0")/functions.sh"
 
+# Detect distro and codename
+. /etc/os-release
+
+if { [[ "$ID" = "debian" && "$VERSION_CODENAME" = "bookworm" ]] }; then
+    check_package_installed "libwebkit2gtk-4.0-37"
+else
+    check_package_installed "libwebkit2gtk-4.1-0"
+fi
+
 check_package_installed "libswt-gtk-4-jni"
-check_package_installed "libwebkit2gtk-4.0-37"
